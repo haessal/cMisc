0e852b2de0a4bf6a524f9b9bb45d5c39c9becf4a
haessal <haessal@64dcb682-3008-11dd-997b-8250fcb5c1fe>
2010-02-08 14:54:15 +0000

    New added
    git-svn-id: http://subversion.assembla.com/svn/phpbookkeeping/cMisc/trunk@76 64dcb682-3008-11dd-997b-8250fcb5c1fe

---
+++ chkEndian.c
@@ -0,0 +1,25 @@
+/*.......1.........2.........3.........4.........5.........6.........*/
+/* Name        : chkEndian.c                                         */
+/* Created on  : 2010/02/08                                          */
+/* Author      : haessal                                             */
+/* Version     :                                                     */
+/* Copyright   : Copyright(C) 2010 haessal. All rights reserved.     */
+/* Description : Check the endian is big or little for 64bit system. */
+/*.......1.........2.........3.........4.........5.........6.........*/
+#include <stdio.h>
+typedef unsigned char  UC;
+typedef unsigned short US;
+typedef unsigned int   UL;
+typedef unsigned long  UW;
+
+int main(int argc, char *argv[])
+{
+	UW boxw = 0xFEDCBA9876543210;
+	US boxs;
+
+	boxs = *((US*)(&boxw));
+
+	printf("uw:%d, ul:%d, us:%d, uc:%d\n",
+			sizeof(UW), sizeof(UL), sizeof(US), sizeof(UC));
+	printf("boxs:0x%x\n", boxs);
+}
