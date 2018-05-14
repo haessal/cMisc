9e46fd92fc4b227a7a95de51be656fa16e8a83c8
haessal <haessal@mizutamauki.net>
2018-05-14 22:21:54 +0900

    Add a target for google test

---
+++ unit-test/gtest_cmisc_main.cpp
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+extern "C"
+{
+#include "../cmisc_main.h"
+}
+
+TEST(cmisc_main, no_parameters)
+{
+	int argc = 1;
+	const char* argv[1] = {"cmisc"};
+	int ret;
+
+	ret = cmisc_main(argc, argv);
+	ASSERT_EQ(0, ret);
+}
