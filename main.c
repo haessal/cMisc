1f831977941d31b5b2a1331481d5a62af4cf090e
haessal <haessal@mizutamauki.net>
2018-05-20 23:44:51 +0900

    Support subcommand

--- main.c
+++ main.c
@@ -1,6 +1,63 @@
+#include <stdlib.h>
+#include <string.h>
+#include "builtin.h"
 #include "main.h"
+
+struct cmd_struct {
+	const char *cmd;
+	int (*fn)(int argc, const char **argv);
+};
+
+static int handle_builtin(int argc, const char **argv);
+static struct cmd_struct *get_builtin(const char *s);
+static int run_builtin(struct cmd_struct *p, int argc, const char **argv);
 
 int cmisc_main(int argc, const char **argv)
 {
-	return 0;
+	int status;
+
+	argv++;
+	argc--;
+	if (argc > 0) {
+		status = handle_builtin(argc, argv);
+	} else {
+		status = EXIT_FAILURE;
+	}
+	return status;
+}
+
+static int handle_builtin(int argc, const char **argv)
+{
+	int status;
+	struct cmd_struct *builtin;
+
+	builtin = get_builtin(argv[0]);
+	if (builtin != NULL) {
+		status = run_builtin(builtin, argc, argv);
+	} else {
+		status = EXIT_FAILURE;
+	}
+	return status;
+}
+
+static struct cmd_struct commands[] = {
+	{ 0, 0}
+};
+static struct cmd_struct *get_builtin(const char *s)
+{
+	int i;
+	struct cmd_struct *p;
+
+	for (i = 0; commands[i].cmd != 0; i++) {
+		p = commands + i;
+		if (!strcmp(s, p->cmd)) {
+			return p;
+		}
+	}
+	return NULL;
+}
+
+static int run_builtin(struct cmd_struct *p, int argc, const char **argv)
+{
+	return p->fn(argc, argv);
 }
