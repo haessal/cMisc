4bfab4a2f28616031d075e84ea0706f6f5c5d96f
haessal <haessal@mizutamauki.net>
2018-05-21 00:01:41 +0900

    wip: try to implement

--- help.c
+++ help.c
@@ -1,7 +1,13 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "builtin.h"
 
 int cmd_help(int argc, const char **argv)
 {
+	int i;
+
+	printf("[help]\n");
+	for (i = 0; i < argc; i++)
+		printf("argv[%d] = %s\n", i, argv[i]);
 	return EXIT_SUCCESS;
 }
